402f5d6438e3e666cc5ee4755234b700d4545e51
Ian Fisher <iafisher@protonmail.com>
2019-04-10 10:53:24 -0400

    Add unit test for textviewer app

--- justhtml/tests.py
+++ justhtml/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 
 
 class JustHTMLTests(TestCase):
-    def test_home_page_uses_right_template(self):
+    def test_index_page_uses_right_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "justhtml/index.html")
--- textviewer/tests.py
+++ textviewer/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TextViewerTests(TestCase):
+    def test_index_page_uses_right_template(self):
+        response = self.client.get("/texts/")
+        self.assertTemplateUsed(response, "textviewer/index.html")
