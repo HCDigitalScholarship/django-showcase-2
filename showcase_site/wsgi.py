b07aec546a3a1ec12ddeb692cf78252112d47e61
Ian Fisher <iafisher@protonmail.com>
2019-04-24 10:12:47 -0400

    Source environment file in showcase_site/wsgi.py

--- showcase_site/wsgi.py
+++ showcase_site/wsgi.py
@@ -13,4 +13,15 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "showcase_site.settings")
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if os.path.exists(os.path.join(BASE_DIR, ".env")):
+    with open(os.path.join(BASE_DIR, ".env"), "r", encoding="utf-8") as f:
+        for line in f:
+            try:
+                key, val = line.strip().split("=", maxsplit=1)
+            except ValueError:
+                pass
+            else:
+                os.environ[key] = val
+
 application = get_wsgi_application()
