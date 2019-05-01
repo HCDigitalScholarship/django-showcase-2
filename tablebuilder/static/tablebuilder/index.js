8d9d8e31bd19889cd249cd2799b040e0f3de09f4
Ian Fisher <iafisher@protonmail.com>
2019-05-01 10:37:31 -0400

    Write add button for tablebuilder

--- tablebuilder/static/tablebuilder/index.js
+++ tablebuilder/static/tablebuilder/index.js
@@ -2,7 +2,47 @@
  * Code to execute when the HTML is loaded. Called at the bottom of this file.
  */
 function onload() {
-    console.log("Hello, world!");
+    let addButton = document.getElementById("add-button");
+    addButton.addEventListener("click", addButtonCallback);
+}
+
+
+function addButtonCallback() {
+    let tbody = document.querySelector("#table tbody");
+    let row = renderNewRow();
+
+    let addButton = document.getElementById("add-button");
+    let addButtonRow = addButton.parentNode.parentNode;
+
+    tbody.insertBefore(row, addButtonRow);
+}
+
+
+function saveButtonCallback() {
+    console.log("Saving!");
+}
+
+
+function renderNewRow() {
+    let row = document.createElement("tr");
+    for (let i = 0; i < 4; i++) {
+        row.appendChild(renderNewCell());
+    }
+
+    let saveButton = document.createElement("button");
+    saveButton.addEventListener("click", saveButtonCallback);
+    saveButton.textContent = "Save";
+    row.appendChild(saveButton);
+    return row;
+}
+
+
+function renderNewCell() {
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    let cell = document.createElement("td");
+    cell.appendChild(input);
+    return cell;
 }
 
 
