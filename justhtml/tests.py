7a12ca914cc64dac996bef14a9c50db5d5e6e486
Ian Fisher <iafisher@protonmail.com>
2019-05-08 10:25:05 -0400

    Intentional failure to test branch protection

--- justhtml/tests.py
+++ justhtml/tests.py
@@ -5,3 +5,6 @@ class JustHTMLTests(TestCase):
     def test_index_page_uses_right_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "justhtml/index.html")
+
+    def test_failure(self):
+        self.assertTrue(False)
